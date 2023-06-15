92e2e0114a04714c17e2f25ce0da8d1c37ddd094
“pallaviathawale” <athawalepallavi1998@gmai.com>
2023-06-15 14:22:10 +0530

    [Pallavi] Add: ConceptOfPolymorphism

---
+++ ObjectOrientedProgrammingConcepts/PolymorphismDemo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedProgrammingConcepts
+{
+    public class PolymorphismDemo
+    {
+        //types of polymorphism
+        //1.Compile Time Polymorphism/Method Overloading
+        public int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public int Add(int num1, int num2, int num3)
+        {
+            return num1 + num2 + num3;
+        }
+
+        //2.Run Time Polymorphism/Method Overriding
+         public virtual  void AnimalSound()
+        {
+            Console.WriteLine("...Animal Makes Sound...");
+        }
+
+        public class Pig : PolymorphismDemo  //derived class
+        {
+            public override void  AnimalSound()
+            {
+                Console.WriteLine("....The pig say wee wee....");
+            }
+        }
+        public class Dog : PolymorphismDemo //derived class
+        {
+            public override void AnimalSound()
+            {
+                Console.WriteLine(".....Barking.....");
+            }
+        }
+    }
+}
--- ObjectOrientedProgrammingConcepts/Program.cs
+++ ObjectOrientedProgrammingConcepts/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace ObjectOrientedProgrammingConcepts
+﻿using static ObjectOrientedProgrammingConcepts.PolymorphismDemo;
+
+namespace ObjectOrientedProgrammingConcepts
 {
     internal class Program
     {
@@ -6,61 +8,19 @@
         {
             Console.WriteLine(".....Welcome to Object Oriented Programming Construct.....");
 
-        //    Food food = new Food("Pizza", "Pasta", "Fish_Fry"); //Create an object of Food class
+            PolymorphismDemo demo = new PolymorphismDemo();
+            int result = demo.Add(22, 18);
+            int result2 = demo.Add(22, 18, 16);
 
-            
-        //    Console.WriteLine("....My Favourite Food....:");   // Access the object's properties and methods
-        //    Console.WriteLine("Junk Food: " + food.Junk_Food);
-        //    Console.WriteLine("Healthy Food:" + food.Healthy_Food);
-        //    Console.WriteLine("Sea Food:" + food.Sea_Food);
-        //   // food.Vegetables();
+            Console.WriteLine(result);
+            Console.WriteLine(result2);
 
-
-        //}
-
-        //    //program with object and class concept
-        //public class Food            //defined class as Food
-        //{
-        //    public string Junk_Food;   //class variables
-        //    public string Healthy_Food;       //class variables
-        //    public string Sea_Food;    //class variables
-
-
-        //    public Food(string J_Food, string H_Food,string S_Food)  // defined constructor
-        //    {
-        //        Junk_Food = J_Food;
-        //        Healthy_Food = H_Food;
-        //        Sea_Food = S_Food;
-        //    }
-
-        //    public void Vegetables()                   //class method
-        //    {
-        //        Console.WriteLine(".........Let's Eat some healthy......");
-        //    }
-        //
-
-
-
-             programmer programmer = new programmer();
-            Console.WriteLine("Salary:" + programmer.salary);
-            Console.WriteLine("Bonus:" + programmer.Bonus);
-
-            Tester tester = new Tester();
-            Console.WriteLine("Salary:" + tester.salary);
-           // Console.WriteLine("Bonus:" + tester.Bonus);
-            Console.WriteLine("Extra Income:" + tester.ExtraIncome);
-
-
-
-            IDrawable drawable;
-            drawable = new Addition();
-            drawable.Operation();
-
-            drawable = new Subtraction();
-            drawable.Operation();
-
-            drawable = new Multiplication();
-            drawable.Operation();
+            PolymorphismDemo animal = new PolymorphismDemo();
+            PolymorphismDemo mypig = new Pig();
+            PolymorphismDemo mydog = new Dog();
+            animal.AnimalSound();
+            mydog.AnimalSound();
+            mypig.AnimalSound();
         }
 
         
