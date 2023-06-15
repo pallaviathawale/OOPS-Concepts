47ba7123d2f6be9834b7b84b7f89c586c343cd2a
“pallaviathawale” <athawalepallavi1998@gmai.com>
2023-06-15 13:40:47 +0530

    [Pallavi Add: ConceptOfInheritance]

---
+++ ObjectOrientedProgrammingConcepts/InheritanceDemo.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedProgrammingConcepts
+{
+    public class InheritanceDemo
+    {
+
+
+    }
+    //Types of Inheritance
+    //1.Single Level Inheritance
+    public class Employee
+    {
+        public float salary = 20000;
+    }
+
+    public class programmer : Employee
+    {
+        public float Bonus = 15000;
+    }
+    // 2.MultiLevel Inheritance
+    //public class Tester : programmer
+    //{
+    //    public float ExtraIncome = 1000;
+    //}
+
+    //3.Heirarchical Inheritance
+
+    public class Tester : Employee
+    {
+        public float ExtraIncome = 1000;
+
+    }
+
+
+    //4.Multiple Inheritance- We can achieve multiple inheritance by using Interface
+
+    public interface IDrawable
+    {
+        void Operation();
+
+    }
+
+    public class Addition :IDrawable
+    {
+        public void Operation()
+        {
+            Console.WriteLine(".....Perform Addition.....");
+        }
+    }
+
+    public class Subtraction : IDrawable
+    {
+        public void Operation()
+        {
+            Console.WriteLine(".....Perform Subtraction.....");
+        }
+    }
+
+    public class Multiplication : IDrawable
+    {
+        public void Operation()
+        {
+            Console.WriteLine(".....Perform Multiplication....");
+        }
+    }
+
+}
--- ObjectOrientedProgrammingConcepts/Program.cs
+++ ObjectOrientedProgrammingConcepts/Program.cs
@@ -6,38 +6,63 @@
         {
             Console.WriteLine(".....Welcome to Object Oriented Programming Construct.....");
 
-            Food food = new Food("Pizza", "Pasta", "Fish_Fry"); //Create an object of Food class
+        //    Food food = new Food("Pizza", "Pasta", "Fish_Fry"); //Create an object of Food class
 
             
-            Console.WriteLine("....My Favourite Food....:");   // Access the object's properties and methods
-            Console.WriteLine("Junk Food: " + food.Junk_Food);
-            Console.WriteLine("Healthy Food:" + food.Healthy_Food);
-            Console.WriteLine("Sea Food:" + food.Sea_Food);
-           // food.Vegetables();
+        //    Console.WriteLine("....My Favourite Food....:");   // Access the object's properties and methods
+        //    Console.WriteLine("Junk Food: " + food.Junk_Food);
+        //    Console.WriteLine("Healthy Food:" + food.Healthy_Food);
+        //    Console.WriteLine("Sea Food:" + food.Sea_Food);
+        //   // food.Vegetables();
 
 
+        //}
+
+        //    //program with object and class concept
+        //public class Food            //defined class as Food
+        //{
+        //    public string Junk_Food;   //class variables
+        //    public string Healthy_Food;       //class variables
+        //    public string Sea_Food;    //class variables
+
+
+        //    public Food(string J_Food, string H_Food,string S_Food)  // defined constructor
+        //    {
+        //        Junk_Food = J_Food;
+        //        Healthy_Food = H_Food;
+        //        Sea_Food = S_Food;
+        //    }
+
+        //    public void Vegetables()                   //class method
+        //    {
+        //        Console.WriteLine(".........Let's Eat some healthy......");
+        //    }
+        //
+
+
+
+             programmer programmer = new programmer();
+            Console.WriteLine("Salary:" + programmer.salary);
+            Console.WriteLine("Bonus:" + programmer.Bonus);
+
+            Tester tester = new Tester();
+            Console.WriteLine("Salary:" + tester.salary);
+           // Console.WriteLine("Bonus:" + tester.Bonus);
+            Console.WriteLine("Extra Income:" + tester.ExtraIncome);
+
+
+
+            IDrawable drawable;
+            drawable = new Addition();
+            drawable.Operation();
+
+            drawable = new Subtraction();
+            drawable.Operation();
+
+            drawable = new Multiplication();
+            drawable.Operation();
         }
 
-            //program with object and class concept
-        public class Food            //defined class as Food
-        {
-            public string Junk_Food;   //class variables
-            public string Healthy_Food;       //class variables
-            public string Sea_Food;    //class variables
-
-
-            public Food(string J_Food, string H_Food,string S_Food)  // defined constructor
-            {
-                Junk_Food = J_Food;
-                Healthy_Food = H_Food;
-                Sea_Food = S_Food;
-            }
-
-            public void Vegetables()                   //class method
-            {
-                Console.WriteLine(".........Let's Eat some healthy......");
-            }
-        }
         
     }
 }
